032222bfc817afdfa6fc684d6d6d72431c94a878
yaouba <kabayyaouba@gmail.com>
2024-12-09 20:45:24 +0100

    Added react toast and the loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import SettingsPage from './pages/SettingsPage'
+import { useAuthStore } from './store/useAuthStore'
+
+import { Toaster } from 'react-hot-toast'
+
+import { Loader } from 'lucide-react'
+import { use } from 'react'
 
 const App = () => {
+  const { user, checkAuth, isCheckingAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  if (isCheckingAuth && !user) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin' />
+      </div>
+    )
+  }
+  
   return (
     <div>
         <Navbar />
 
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/signup' element={<SignupPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/' element={ user ? <HomePage /> : <Navigate to='/login' />} />
+          <Route path='/signup' element={!user ? <SignupPage /> : <Navigate to='/' />} />
+          <Route path='/login' element={!user ? <LoginPage /> : <Navigate to='/' />} />
+          <Route path='/profile' element={user ? <ProfilePage /> : <Navigate to='/login' />} />
           <Route path='/settings' element={<SettingsPage />} />
 
         </Routes>
+
+        <Toaster />
     </div>
   )
 }

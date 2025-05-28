3f1d6124fa31ce2f8463d8d1dbea1e4264b6f8a8
yaouba <kabayyaouba@gmail.com>
2025-05-28 15:42:47 +0100

    feat(ui): created the chat page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import ChatContainer from '../components/ChatContainer'
+import NoChatSelected from '../components/NoChatSelected'
+import Sidebar from '../components/Sidebar'
+import { useChatStore } from '../store/useChatStore'
 
 function HomePage() {
+  const { selectedUser } = useChatStore()
+
   return (
-    <div>HomePage</div>
+    <div className='h-screen bg-base-200'>
+      <div className='flex items-center justify-center pt-20 px-4'>
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+
+            {selectedUser ? <ChatContainer /> : <NoChatSelected />}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

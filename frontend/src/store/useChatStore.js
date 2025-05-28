e4d3248e97e8ad5c1177de8b8efcb20853f6c852
yaouba <kabayyaouba@gmail.com>
2025-05-28 15:43:53 +0100

    feat(ui): add chat store

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,50 @@
+import { create } from 'zustand'
+import { apiCall } from '../lib/axios'
+import toast from 'react-hot-toast';
+
+export const useChatStore = create((set, get) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    isSendingMessage: false,
+
+    getUsers: async () => {
+        set({ isUsersLoading: true })
+        try {
+            const res = await apiCall.get('/messages/users');
+            set({ users: res.data.data })
+        } catch (err) {
+            toast.error(err.response.data.message)
+        } finally {
+            set({ isUsersLoading: false })
+        }
+    },
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true })
+        try {
+            const res = await apiCall.get(`/messages/${userId}`);
+            set({ messages: res.data.data })
+        } catch (err) {
+            toast.error(err.response.data.message)
+        } finally {
+            set({ isMessagesLoading: false })
+        }
+    },
+    sendMessage: async (data) => {
+        const { messages, selectedUser } = get()
+        set({ isSendingMessage: true })
+        try {
+            const res = await apiCall.post(`/messages/send/${selectedUser?._id}`, data);
+            set({ messages: [...messages, res.data.data] })
+        } catch (err) {
+            toast.error(err.response.data.message)
+        } finally {
+            set({ isSendingMessage: false })
+        }
+    },
+    setSelectedUser: (selectedUser) => {
+        set({ selectedUser })
+    },
+}))
